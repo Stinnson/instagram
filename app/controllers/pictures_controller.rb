528978240fa294be429f674e96edd2e74019edc9
Mark Terenz <dasso007@hotmail.com>
2018-10-18 08:35:59 +0200

    Create new pics actions and view files

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,15 +1,36 @@
 class PicturesController < ApplicationController
 
   def index
+
   end
 
   def show
+
   end
 
   def new
+    @pic = Picture.new
   end
 
   def create
+    @pic = Picture.new(pic_params)
   end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def destroy
+  end
+
+  private
+
+  def pic_params
+    params.require(:picture).permit(:title, :description)
+  end
+
 
 end
