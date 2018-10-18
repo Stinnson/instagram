d2fcec5fbd69f52af15bb1c1dffdb309f7b05ee4
Mark Terenz <dasso007@hotmail.com>
2018-10-18 09:19:52 +0200

    Edit update and destroy file

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,11 +1,11 @@
 class PicturesController < ApplicationController
+  before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
   def index
-
+    @pics = Picture.all.order("created_at DESC")
   end
 
   def show
-
   end
 
   def new
@@ -14,16 +14,30 @@ class PicturesController < ApplicationController
 
   def create
     @pic = Picture.new(pic_params)
+    if @pic.save
+      flash[:success] = "Yaye! Your picture is posted!"
+      redirect_to @pic
+    else
+      render "new"
+    end
   end
 
   def edit
+
   end
 
   def update
-
+    if @pic.update(pic_params)
+      flash[:success] = "Your picture was updated"
+      redirect_to @pic
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @pic.destroy
+    redirect_to root_path
   end
 
   private
@@ -32,5 +46,8 @@ class PicturesController < ApplicationController
     params.require(:picture).permit(:title, :description)
   end
 
+  def find_pic
+    @pic = Picture.find(params[:id])
+  end
 
 end
